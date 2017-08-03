970065ce42dae59f68cc269ce8e8dec79438ed3b
Julian Herrera <julian.herrera@live.com>
2017-08-03 13:35:22 -0500

    initial commit

---
+++ src/components/Product.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Product extends Component {
+    render() {
+        const {product} = this.props;
+
+        return (
+            <li>
+              <img src={product.image}></img>
+              <h3>{product.title}</h3>
+              <h4>{product.price}</h4>
+            </li>
+        )
+    }
+}
+
+export default Product;
